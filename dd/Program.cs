b6483cf3749c41b59a9fd94e949f451e96a30017
yasya <kalgexavva1970@icloud.com>
2022-12-04 10:55:06 +0300

    add new fole

---
+++ dd/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
